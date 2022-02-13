4ee32e21680cb796eef3edf0f12cbf06c05dae8b
Rakibul Hassan Nayon <rakibul.nayon@outlook.com>
2022-02-13 21:04:16 +0600

    adding and decuction

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log('I am in a separate file')
+function updateCaseNumber(IsIncreasing) {
+    const caseInput = document.getElementById("case-number");
+    let caseNumber = caseInput.value;
+    if (IsIncreasing == true) {
+        caseNumber.value = parseInt(caseNumber) + 1;
+    } else if (caseNumber > 0) {
+        caseNumber.value = parseInt(caseNumber) - 1;
+    }
+    caseInput.value = caseNumber;
+    //Update Case Total
+    const caseTotal = document.getElementById("case-total");
+    caseTotal.innerText = caseNumber * 59;
+}
+
+document.getElementById("case-plus").addEventListener("click", function() {
+    updateCaseNumber(true);
+});
+
+document.getElementById("case-minus").addEventListener("click", function() {
+    updateCaseNumber(false);
+});
